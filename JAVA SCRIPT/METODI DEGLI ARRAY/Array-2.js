fd935ab896eb0af51552c7c32537326e49cbb132
Tommy <tommaso.merli97@gmail.com>
2023-11-08 15:22:44 +0100

    Esercizio svolto in pair assieme a Giuseppe

--- JAVA SCRIPT/METODI DEGLI ARRAY/Array-2.js
+++ JAVA SCRIPT/METODI DEGLI ARRAY/Array-2.js
@@ -1,82 +1,92 @@
 
 //Implementare la funzione firstUncompletedNote che, dato un array di note, restituisce la prima nota non completata. Una nota viene considerata completata se tutti i todo presenti hanno il flag done impostato a true.
 
-function firstUncompletedNote(notes) {
-    const findTodos = notes.find(note => note.todos);
-        return notes.find(check => check.done != true);
+const notes = [
+  {
+    id: 1,
+    description: 'Workout program',
+    todos: [
+      {
+        id: 1,
+        name: 'Push ups - 10 x 3',
+        done: true
+      },
+      {
+        id: 2,
+        name: 'Abdominals - 20 x 3',
+        done: true
+      },
+      {
+        id: 3,
+        name: 'Tapis Roulant - 15min',
+        done: true
+      }
+    ]
+  },
+  {
+    id: 2,
+    description: 'Front-end Roadmap',
+    todos: [
+      {
+        id: 1,
+        name: 'Learn HTML',
+        done: true
+      },
+      {
+        id: 2,
+        name: 'Learn CSS',
+        done: false
+      },
+      {
+        id: 3,
+        name: 'Learn JavaScript',
+        done: true
+      },
+      {
+        id: 4,
+        name: 'Learn Angular',
+        done: false
+      }
+    ]
+  },
+  {
+    id: 3,
+    description: 'My Movies Library',
+    todos: [
+      {
+        id: 1,
+        name: 'La città incantata',
+        done: true
+      },
+      {
+        id: 2,
+        name: 'Avatar',
+        done: true
+      },
+      {
+        id: 3,
+        name: 'Titanic',
+        done: false
+      }
+    ]
+  }
+]
+
+// ESEMPIO 1, CALLBACK COSTRUITA ESTERNAMENTE:
+
+function firstUncompletedNote(par) {
+  const uncompletedNote = par.find(element => element.todos.find(element => element.done === false));
+  return uncompletedNote
 }
-  
-  const notes = [
-    {
-      id: 1,
-      description: 'Workout program',
-      todos: [
-        {
-          id: 1,
-          name: 'Push ups - 10 x 3',
-          done: true
-        },
-        {
-          id: 2,
-          name: 'Abdominals - 20 x 3',
-          done: true
-        },
-        {
-          id: 3,
-          name: 'Tapis Roulant - 15min',
-          done: true
-        }
-      ]
-    },
-    {
-      id: 2,
-      description: 'Front-end Roadmap',
-      todos: [
-        {
-          id: 1,
-          name: 'Learn HTML',
-          done: true
-        },
-        {
-          id: 2,
-          name: 'Learn CSS',
-          done: false
-        },
-        {
-          id: 3,
-          name: 'Learn JavaScript',
-          done: true
-        },
-        {
-          id: 4,
-          name: 'Learn Angular',
-          done: false
-        }
-      ]
-    },
-    {
-      id: 3,
-      description: 'My Movies Library',
-      todos: [
-        {
-          id: 1,
-          name: 'La città incantata',
-          done: true
-        },
-        {
-          id: 2,
-          name: 'Avatar',
-          done: true
-        },
-        {
-          id: 3,
-          name: 'Titanic',
-          done: false
-        }
-      ]
-    }
-  ]
-  
-  const noteInProgress = firstUncompletedNote(notes);
-  console.log('All notes: ', notes); 
-  console.log('First note in progress: ', noteInProgress);
+
+const noteInProgress = firstUncompletedNote(notes);
+console.log('All notes: ', notes); 
+console.log('First note in progress: ', noteInProgress);
+
+// ESEMPIO 2, FUNZIONE ANONIMA:
+
+const firstUncompletedNote = notes.find(element => element.todos.find(element => element.done === false));
+
+console.log(firstUncompletedNote);
+
+// ESERCIZIO ESEGUITO IN PAIR PROGRAMMING DA GIUSEPPE E TOMMASO.
