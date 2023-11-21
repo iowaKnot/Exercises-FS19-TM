509474ad7b6c9cb97402233f263404ce10ffe54b
Tommy <tommaso.merli97@gmail.com>
2023-11-21 15:06:49 +0100

    Completamento esercizio Callback 4

--- JAVA SCRIPT/CALLBACK/cb_4.js
+++ JAVA SCRIPT/CALLBACK/cb_4.js
@@ -3,20 +3,22 @@
 // La funzione dovrebbe eseguire ogni callback nell'ordine in cui appaiono nell'array.
 
 function runCallbacks(callbacks) {
-    
-};
+    callbacks.forEach(callback => {
+        callback();
+    });
+}
 
 function firstCallback() {
     console.log("This is the first callback!");
-};
+}
 
 function secondCallback() {
     console.log("This is the second callback!");
-};
+}
 
 function thirdCallback() {
     console.log("This is the third callback!");
-};
+}
 
 const callbackArray = [firstCallback, secondCallback, thirdCallback];
 runCallbacks(callbackArray);
